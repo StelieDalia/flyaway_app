3b3be554bb1104ea681eada5ab8963ac3f9403fe
Leilamouk <leilamoukagni@gmail.com>
2022-10-18 20:50:52 +0100

    inte

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react' ;
 import avionun  from '../assets/avionun.png'
 import {Link}  from 'react-router-dom'
 import mlogo from '../assets/mlogo.jpeg'
+import './Home.css';
 
 
 
@@ -12,8 +13,8 @@ export default function Home () {
         <img className='first' src={avionun} alt="image"></img>
         <div className='mes'>
           <h3 className='tex'>Mon billet d'avion <br></br> <span className='texo'>en quelques clics ! </span> </h3>
-            <button className='button'><Link to="/connexion">S'inscrire</Link></button>
-            <button className='buttons'><Link to="/Reserve">Rechercher</Link></button>
+            <button className='btn'><Link to="/connexion">S'inscrire</Link></button>
+            <button className='btns'><Link to="/Reserve">Rechercher</Link></button>
            
         </div>
     </div>
--- src/components/Vols.js
+++ src/components/Vols.js
@@ -5,6 +5,7 @@ import logo from './images/logo.png'
 import ethiopian from './images/ethiopian.png'
 import airfrance from './images/airfrance.png'
 import './Vols.css';
+import { Link } from 'react-router-dom';
 
 
 function BgColorExample() {
@@ -13,67 +14,77 @@ function BgColorExample() {
     <div className='cards'>
       
       <Card className='card'>
-        <Card.Header className='turkish'><img src={Turkish_Airlines} height="35"/> <h4>220 400 Fcfa</h4></Card.Header>
-        <Card.Body>
-          <div className='text'>
-            <Card.Text>
-            LBV <br/> 06:30 AM
-            </Card.Text>
-            <div className='plane'><img src={airplane} height="15"/><h6>7h 10m</h6></div>
-            <Card.Text>
-            IST <br/> 03:30 PM
-            </Card.Text>
-          </div>
-          <Card.Title className='detail'><h6>Détails & frais bagages</h6></Card.Title>
-        </Card.Body>
+        <Link to="/enregistrement" className='link'>
+          <Card.Header className='turkish'><img src={Turkish_Airlines} height="35"/> <h4>220 400 Fcfa</h4></Card.Header>
+          <Card.Body>
+            <div className='text'>
+              <Card.Text>
+              LBV <br/> 06:30 AM
+              </Card.Text>
+              <div className='plane'><img src={airplane} height="15"/><h6>7h 10m</h6></div>
+              <Card.Text>
+              IST <br/> 03:30 PM
+              </Card.Text>
+            </div>
+            <Card.Title className='detail'><h6>Détails & frais bagages</h6></Card.Title>
+          </Card.Body>
+        </Link>
+      </Card>
+
+      
+        <Card className='card'>
+          <Link to="/enregistrement" className='link'>
+            <Card.Header className='logo'><img src={logo} height="45"/> <h4> 50 000 Fcfa</h4></Card.Header>
+            <Card.Body>
+              <div className='text'>
+                <Card.Text>
+                LBV <br/> 06:30 AM
+                </Card.Text>
+                <div className='plane'><img src={airplane} height="15"/><h6>45min</h6></div>
+                <Card.Text>
+                POG <br/> 03:30 PM
+                </Card.Text>
+              </div>
+              <Card.Title className='detail'><h6>Détails & frais bagages</h6></Card.Title>
+            </Card.Body>
+          </Link>
+        </Card>
+      
+
+      <Card className='card'>
+        <Link to="/enregistrement" className='link'>
+          <Card.Header className='ethiopian'><img src={ethiopian} height="35"/> <h4>355 000 Fcfa</h4></Card.Header>
+          <Card.Body>
+            <div className='text'>
+              <Card.Text>
+              LBV <br/> 06:30 AM
+              </Card.Text>
+              <div className='plane'><img src={airplane} height="15"/><h6>7h 10m</h6></div>
+              <Card.Text>
+              DAK <br/> 03:30 PM
+              </Card.Text>
+            </div>
+            <Card.Title className='detail'><h6>Détails & frais bagages</h6></Card.Title>
+          </Card.Body>
+        </Link>
       </Card>
 
       <Card className='card'>
-        <Card.Header className='logo'><img src={logo} height="45"/> <h4> 50 000 Fcfa</h4></Card.Header>
-        <Card.Body>
-          <div className='text'>
-            <Card.Text>
-            LBV <br/> 06:30 AM
-            </Card.Text>
-            <div className='plane'><img src={airplane} height="15"/><h6>45min</h6></div>
-            <Card.Text>
-            POG <br/> 03:30 PM
-            </Card.Text>
-          </div>
-          <Card.Title className='detail'><h6>Détails & frais bagages</h6></Card.Title>
-        </Card.Body>
-      </Card>
-
-      <Card className='card'>
-        <Card.Header className='ethiopian'><img src={ethiopian} height="35"/> <h4>355 000 Fcfa</h4></Card.Header>
-        <Card.Body>
-          <div className='text'>
-            <Card.Text>
-            LBV <br/> 06:30 AM
-            </Card.Text>
-            <div className='plane'><img src={airplane} height="15"/><h6>7h 10m</h6></div>
-            <Card.Text>
-            DAK <br/> 03:30 PM
-            </Card.Text>
-          </div>
-          <Card.Title className='detail'><h6>Détails & frais bagages</h6></Card.Title>
-        </Card.Body>
-      </Card>
-
-      <Card className='card'>
-        <Card.Header className='airfrance'><img src={airfrance} height="45"/> <h4>550 400 Fcfa</h4></Card.Header>
-        <Card.Body>
-          <div className='text'>
-            <Card.Text>
-            LBV <br/> 06:30 AM
-            </Card.Text>
-            <div className='plane'><img src={airplane} height="15"/><h6>7h 10m</h6></div>
-            <Card.Text>
-            PAR <br/> 03:30 PM
-            </Card.Text>
-          </div>
-          <Card.Title className='detail'><h6>Détails & frais bagages</h6></Card.Title>
-        </Card.Body>
+        <Link to="/enregistrement" className='link'>
+          <Card.Header className='airfrance'><img src={airfrance} height="45"/> <h4>550 400 Fcfa</h4></Card.Header>
+          <Card.Body>
+            <div className='text'>
+              <Card.Text>
+              LBV <br/> 06:30 AM
+              </Card.Text>
+              <div className='plane'><img src={airplane} height="15"/><h6>7h 10m</h6></div>
+              <Card.Text>
+              PAR <br/> 03:30 PM
+              </Card.Text>
+            </div>
+            <Card.Title className='detail'><h6>Détails & frais bagages</h6></Card.Title>
+          </Card.Body>
+        </Link>
       </Card>
 
     </div>
